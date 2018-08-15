d6e496dc3638d2e3ce2c3e9b3f00f50699f66d33
UDgenerate <42360597+UDgenerate@users.noreply.github.com>
2018-08-14 19:30:31 -0500

    Now scrapes prices and links on book page

--- book_scraper/main.py
+++ book_scraper/main.py
@@ -9,11 +9,23 @@ def books_spider(max_pages):
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text, "html.parser")
         for link in soup.findAll(lambda tag: len(tag.name) == 1 and len(tag.attrs) == 2):
-            href = "http://books.toscrape.com/catalogue"  + link.get('href')[5:]
+            href = "http://books.toscrape.com/catalogue" + link.get('href')[5:]
             title = link.get('title')
             print(title)
             print(href)
+            get_single_item_data(href)
+
         page += 1
 
-books_spider(1)
+def get_single_item_data(item_url):
+    source_code = requests.get(item_url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, "html.parser")
+    for item_price in soup.findAll('p', {'class': 'price_color'}):
+        print(item_price.string[1:])
+    for link in soup.findAll('a'):
+        href = "http://books.toscrape.com/catalogue" + link.get('href')[2:]
+        print(href)
 
+
+books_spider(1)
